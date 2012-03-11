ac52ab87bdc10588e0355c0529227d2ed8745673
sizkin <sizkin@gmail.com>
2012-03-11 03:20:50 +0000

    "create the function to get HTTPheader informations"

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 var express = require('express'),
 	app = module.exports = express.createServer();
+var http = require('http');
 
 // custom module
 //
 
-// mongodb connection
-var Db = require('mongodb').Db,
-	Connection = require('mongodb').Connection,
-	Server = require('mongodb').Server,
-	BSON = require('mongodb').BSON,
-	ObjectID = require('mongodb').ObjectID;
+// global var
+var port = process.env.C9_PORT || 3000;
 
-var db = new Db('fyp', new Server('localhost', 27017, {}, {}));
+// mongodb connection
+//var Db = require('mongodb').Db,
+//	Connection = require('mongodb').Connection,
+//  Server = require('mongodb').Server,
+//	BSON = require('mongodb').BSON,
+//	ObjectID = require('mongodb').ObjectID;
+
+//var db = new Db('fyp', new Server('localhost', 27017, {}, {}));
 // End mongodb connection
 
 app.configure(function() {
@@ -35,13 +39,25 @@ app.get('/', function(req, res) {
 
 // Set the params for request
 app.param('urlT');
+app.param('host');
 
-app.get('/:urlT', function() {
+app.get('/:urlT', function(req, res) {
 	var urlT = req.params.urlT;
 	res.render(urlT, {
 		title: 'WAF System - ' + urlT
 	});
 });
 
-app.listen(3000);
+// Get the informations of Server
+var options = {
+    host: 'www.apache.org',
+    port: 80,
+    path: '/'
+};
+http.get(options, function(req) {
+    var serverName = req.header('Server');
+    console.log(serverName);
+});
+
+app.listen(port);
 console.log('Server is running');
